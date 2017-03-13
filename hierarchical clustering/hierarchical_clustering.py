af4c282b2ab5acaf814678b9854705902ec441c8
Grivladan <grigorenkovlad1993@gmail.com>
2017-03-13 11:52:25 +0200

    generate random data

---
+++ hierarchical clustering/hierarchical_clustering.py
@@ -0,0 +1,14 @@
+from matplotlib import pyplot as plt
+from scipy.cluster.hierarchy import dendrogram, linkage
+import numpy as np
+
+np.set_printoptions(precision=5, suppress=True)
+
+# generate two clusters: a with 100 points, b with 50:
+np.random.seed(4711)  # for repeatability of this tutorial
+a = np.random.multivariate_normal([10, 0], [[3, 1], [1, 4]], size=[100,])
+b = np.random.multivariate_normal([0, 20], [[3, 1], [1, 4]], size=[50,])
+X = np.concatenate((a, b),)
+print(X.shape)  # 150 samples with 2 dimensions
+plt.scatter(X[:,0], X[:,1])
+plt.show()
