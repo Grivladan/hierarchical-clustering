d164af4ba5422dcb431a69a150391709669e90c6
Grivladan <grigorenkovlad1993@gmail.com>
2017-03-13 12:10:01 +0200

    build dendrogram with scipy

--- hierarchical clustering/hierarchical_clustering.py
+++ hierarchical clustering/hierarchical_clustering.py
@@ -5,10 +5,23 @@ import numpy as np
 np.set_printoptions(precision=5, suppress=True)
 
 # generate two clusters: a with 100 points, b with 50:
-np.random.seed(4711)  # for repeatability of this tutorial
-a = np.random.multivariate_normal([10, 0], [[3, 1], [1, 4]], size=[100,])
-b = np.random.multivariate_normal([0, 20], [[3, 1], [1, 4]], size=[50,])
+np.random.seed(500)  # for repeatability of this tutorial
+a = np.random.multivariate_normal([10, 0], [[3, 1], [1, 4]], size=[10,])
+b = np.random.multivariate_normal([0, 20], [[3, 1], [1, 4]], size=[5])
 X = np.concatenate((a, b),)
 print(X.shape)  # 150 samples with 2 dimensions
 plt.scatter(X[:,0], X[:,1])
+
+Z = linkage(X, 'ward')
+
+# calculate full dendrogram
+plt.figure(figsize=(25, 10))
+plt.title('Hierarchical Clustering Dendrogram')
+plt.xlabel('sample index')
+plt.ylabel('distance')
+dendrogram(
+    Z,
+    leaf_rotation=90.,  # rotates the x axis labels
+    leaf_font_size=8.,  # font size for the x axis labels
+)
 plt.show()
